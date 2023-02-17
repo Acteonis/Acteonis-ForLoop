ec4302b766812c79abb8ba6f775ec611906c301a
Acteonis <adosminkina@gmail.com>
2023-02-17 23:19:51 +0000

    The Lab is completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -41,6 +41,7 @@ public class Lab {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
+        for (int i =start; i<end; i++){buildingString = buildingString +i +" ";}
 
 //        code here
 
